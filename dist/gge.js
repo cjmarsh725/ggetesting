65771774d483e6f8ceec986a23e7bde1afe64e0d
Chris <cjmarsh725@gmail.com>
2019-07-01 15:42:29 -0700

    Rollup testing

---
+++ dist/gge.js
@@ -0,0 +1,16 @@
+var GG = (function (exports) {
+  'use strict';
+
+  const config = () => {
+    return "foobar";
+  };
+
+  const setup = () => {
+    console.log(config());
+  };
+
+  exports.setup = setup;
+
+  return exports;
+
+}({}));
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = () => {
+  return "foobar";
+}
+
+export default config;
